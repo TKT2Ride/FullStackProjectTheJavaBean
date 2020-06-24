30aaccd3fe973cb34fb3b13c392b5fc6d04aff03
Tyler K. Tran <tran.kiet.tyler@gmail.com>
2020-06-24 11:43:36 -0500

    init

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/App_Start/Startup.Auth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using CTWMasterClass_WebAppActivities.Models;
+
+namespace CTWMasterClass_WebAppActivities
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+           
+        }
+    }
+}
---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿//using CTWMasterClass_WebAppActivities.Models.Entity;
+using System.Data.Entity;
+using System.Reflection;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CTWMasterClass_WebAppActivities.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
