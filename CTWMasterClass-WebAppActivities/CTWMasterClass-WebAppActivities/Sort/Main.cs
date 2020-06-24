d94e49c1300b73d58e8906626638acd885131cbe
Vaishnavi Manikonda <vaishnavi.manikonda@outlook.com>
2020-06-24 14:49:10 -0500

    Added Barrel class

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Sort/Main.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class GFG
+{
+
+    // Main Method 
+    static public void Main(String[] args)
+    {
+
+        Console.WriteLine("Main Method");
+    }
+}
---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Sort/Sort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CTWMasterClass_WebAppActivities.Sort
+{
+    public class Sort: IComparer<Barrel>
+    {
+        public int Compare(Barrel x, Barrel y)
+        {
+            return 0;
+        }
+    }
+}
