1efde4611a6f754ff4dbd3db51a86af0a4487271
Preston Tran <22tranp@elmbrookstudents.org>
2020-06-24 19:22:03 -0600

    Added Inputs to the Create and Edit Views. Removed Layout headings, created a Barrel controller

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/BarrelController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CTWMasterClass_WebAppActivities.Controllers
+{
+    public class BarrelController : Controller
+    {
+        private BarrelService service = new BarrelService();
+        // GET: Barrel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
